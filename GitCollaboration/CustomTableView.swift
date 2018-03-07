d1747e3d4f3096117b5078ef06e6a6c662083ead
Kevin T <kevin.tai.mail@gmail.com>
2018-03-06 20:23:32 -0800

    dev b forked it

---
+++ GitCollaboration/CustomTableView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableView.swift
+//  GitCollaboration
+//
+//  Created by KevinT on 2018-03-06.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableView: UITableViewController {
+  
+}
+
+class CustomViewController: UIViewController {
+  
+}
