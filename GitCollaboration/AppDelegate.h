0b4f919aaf5f5efeb0a9743cde6b3ae23c5a5dae
Christoper <celoranta@gmail.com>
2018-03-06 19:02:10 -0800

    boilerplate

---
+++ GitCollaboration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCollaboration
+//
+//  Created by Chris Eloranta on 2018-03-06.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCollaboration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCollaboration
+//
+//  Created by Chris Eloranta on 2018-03-06.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
